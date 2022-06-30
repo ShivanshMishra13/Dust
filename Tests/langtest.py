704d44b18b9882fe1a781b696355f4a3c90689fd
ShivanshMishra13 <107501685+ShivanshMishra13@users.noreply.github.com>
2022-06-30 14:00:02 +0530

    Add files via upload

--- Tests/langtest.py
+++ Tests/langtest.py
@@ -1,7 +1,21 @@
-from DustBK import Run
+from Dust import Run
 
 while True:
-	text= input("Dust > ")	
+	text= input("Dust > ")
+	if text.strip()=="": continue	
 	result,error=Run(text,"<stdin>") 
 	if error: print(error.in_string()) 
-	else: print(result)
+	elif result:
+	    if len(result.elements)==1:
+	        print(result.elements[0]) 
+	    else: print(result)
+#. "Hello World"
+#func a(x,y)->x+y
+#var s=[8]
+#add([69],82)
+#ttbnc([8,9,6],7)
+#var s= if 5==5 then "laude" else "fuck"
+#if 5==5 then; print("fuck boi");print("noob bitch ") else print("wtf")
+# func sub(x,y);print("subrtracting");print("noob") ;end 
+#for i=0 to 10 then ; print(i);print("bitch") end
+#while i<10 then ; print("bicth");print(i) end 
